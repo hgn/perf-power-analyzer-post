0178e139d6f884a63d3d681c7f8176bbcd63d7f8
Hagen Paul Pfeifer <hagen@jauu.net>
2024-01-04 22:07:58 +0100

    timer pie: update
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- images/timer-01/timer-type-callback-expiration-pie.py
+++ images/timer-01/timer-type-callback-expiration-pie.py
@@ -1,64 +1,33 @@
 #!/usr/bin/python3
 
+import os
+import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
 
-file_path = 'timer-type-callback-expiration-pie.txt'
+FILE_BASE = os.path.basename(__file__).rstrip('.py')
+FILE_DATA = FILE_BASE + ".txt"
+FILE_PNG  = FILE_BASE + ".png"
+FILE_PDF  = FILE_BASE + ".pdf"
 
-# Read data from file
-df = pd.read_csv(file_path, delim_whitespace=True)
+colors = ['#3498db', '#e74c3c']
+lineprops = {"linewidth": 1, "edgecolor": "white"}
 
-# Group by TimerType and sum the Expires values
+df = pd.read_csv(FILE_DATA, delim_whitespace=True)
 grouped_data = df.groupby('TimerType')['Expires'].sum()
 
-# Creating a pie chart
-fig, ax = plt.subplots()
-ax.pie(grouped_data, labels=grouped_data.index, autopct='%1.1f%%', startangle=90)
-ax.axis('equal')  # Equal aspect ratio ensures the pie chart is circular.
+def label_print(pct, allvals):
+    absolute = int(round(pct/100.*sum(allvals)))
+    return "{:.1f}%\n({:d})".format(pct, absolute)
 
-plt.title('Timer Counts by Type')
-plt.show()
+fig, ax = plt.subplots(figsize=(5, 5))
+ax.pie(grouped_data, labels=grouped_data.index,
+        autopct=lambda pct: label_print(pct, grouped_data),
+        startangle=90, colors=colors, wedgeprops=lineprops)
+ax.axis('equal')
 
-#  # since column <n>, the data starts
-#  DATA_COLUMN = 2
-#  
-#  data = pd.read_csv("timer-type-callback-expiration.txt", sep='\s+', header=0)
-#  max_value = data.iloc[:, DATA_COLUMN:].max().max()
-#  data = data.head(30)
-#  no_tasks = data.shape[0]
-#  
-#  fig, axs = plt.subplots(no_tasks, 1, sharex=True, figsize=(30, 20))
-#  for i, data in enumerate(data.iterrows()):
-#      index, row = data
-#      irq_no = row.iloc[0]
-#      irq_name = row.iloc[1]
-#      row_mean = row.iloc[DATA_COLUMN:].mean()
-#      row_max = row.iloc[DATA_COLUMN:].max()
-#      reshaped_array = np.array(row.iloc[DATA_COLUMN:].tolist()).reshape((1, -1))
-#      im = axs[i].imshow(reshaped_array, cmap=plt.cm.viridis_r, extent=[0, 10, 0, 10],
-#                         aspect='auto', interpolation='none', vmin=0, vmax=max_value)
-#      # I use TeX here for boldface font generatiom, remove textbf and usetex=False
-#      # to get rid of it
-#      text = f"\\textbf{{{irq_name } - {irq_no}}}\nMean:{row_mean:.1f}  Max:{row_max}"
-#      axs[i].text(-1, 5, text, multialignment='left', va='center', ha='left', fontsize=12, usetex=True)
-#  
-#  # add colorbar legend
-#  cbar_ax = fig.add_axes([0.95, 0.1, 0.03, 0.8])
-#  cbar = fig.colorbar(im, cax=cbar_ax)
-#  
-#  # remove borders and make is look more modern
-#  for ax in axs:
-#      ax.set_xticks([])
-#      ax.set_yticks([])
-#      ax.set_xticklabels([])
-#      ax.set_yticklabels([])
-#      ax.spines['top'].set_visible(False)
-#      ax.spines['right'].set_visible(False)
-#      ax.spines['bottom'].set_visible(False)
-#      ax.spines['left'].set_visible(False)
-#  
-#  
-#  plt.savefig('wakeups-timemap-irq.pdf')
-#  plt.savefig('wakeups-timemap-irq.png')
+print(f'generate {FILE_PDF}')
+plt.savefig(FILE_PDF, dpi=300, bbox_inches='tight')
+print(f'generate {FILE_PNG}')
+plt.savefig(FILE_PNG, dpi=300, bbox_inches='tight')
