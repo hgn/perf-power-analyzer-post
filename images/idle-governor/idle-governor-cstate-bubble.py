ca6f2b1de3337a8cb4a4e2348b0f8da59467a8ff
sploityx <florian.aschenbrenner@posteo.de>
2024-01-09 23:56:29 +0100

    idle-governor: bubble small cleanup

--- images/idle-governor/idle-governor-cstate-bubble.py
+++ images/idle-governor/idle-governor-cstate-bubble.py
@@ -12,16 +12,7 @@ FILE_PDF  = FILE_BASE + ".pdf"
 
 colors = iter([plt.cm.tab10(i) for i in range(20)])
 
-df = pd.read_csv(FILE_DATA, delim_whitespace=True, usecols=['C-State', 'Miss', 'Below', 'Sleep[ns]', 'CPU'])
-df = df.replace(to_replace='-', value=0)
-df['Below'] = pd.to_numeric(df['Below'], errors='coerce', downcast='integer')
-
-grouped_data = df.groupby(by=['C-State', 'CPU']).agg({'Miss' : 'sum', 'Below' : 'sum', 'C-State' : 'size', 'Sleep[ns]' : 'sum'})
-grouped_data = grouped_data.rename(columns={'C-State': 'Entries'}).reset_index()
-grouped_data['Above'] = grouped_data['Miss'] - grouped_data['Below']
-grouped_data['Match'] = grouped_data['Entries'] - grouped_data['Miss']
-grouped_data = grouped_data.drop(columns = ['Miss', 'Entries'])
-
+df = pd.read_csv(FILE_DATA, delim_whitespace=True, usecols=['C-State', 'Sleep[ns]', 'CPU'])
 
 for cpu in df['CPU'].unique():
     sleep_data = (df.loc[df['CPU']==cpu])
@@ -31,7 +22,6 @@ for cpu in df['CPU'].unique():
 plt.xlabel('Sequence')
 plt.ylabel('Sleep [ns]')
 plt.legend()
-
 
 print(f'generate {FILE_PDF}')
 plt.savefig(FILE_PDF, dpi=300, bbox_inches='tight')
