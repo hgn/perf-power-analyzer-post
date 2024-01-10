295981f08bcd2fd7506a899c3dd0c3a7c58c2449
sploityx <florian.aschenbrenner@posteo.de>
2024-01-10 12:38:20 +0100

    idle-governor: add cpu violin plot

---
+++ images/idle-governor/idle-governor-cpu-violin.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+FILE_BASE = os.path.basename(__file__).rstrip('.py')
+FILE_DATA = FILE_BASE + ".txt"
+FILE_PNG  = FILE_BASE + ".png"
+FILE_PDF  = FILE_BASE + ".pdf"
+
+colors = iter([plt.cm.tab10(i) for i in range(20)])
+
+df = pd.read_csv(FILE_DATA, delim_whitespace=True, usecols=['CPU', 'Sleep[ns]'])
+
+columns = []
+positions = []
+for cpu in df['CPU'].unique():
+    sleep_data = df[df['CPU']==cpu]['Sleep[ns]']
+    columns.append(sleep_data)
+    positions.append(cpu)
+
+plt.violinplot(columns, positions=positions, showmedians=True)
+
+plt.xlabel('CPU')
+plt.ylabel('Sleep [ns]')
+plt.yscale('log')
+
+print(f'generate {FILE_PDF}')
+plt.savefig(FILE_PDF, dpi=300, bbox_inches='tight')
+print(f'generate {FILE_PNG}')
+plt.savefig(FILE_PNG, dpi=300, bbox_inches='tight')
