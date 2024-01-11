75447d63bfdd3211f563014bc3e66920ab17d5cf
sploityx <florian.aschenbrenner@posteo.de>
2024-01-11 11:11:07 +0100

    idle-governor: added cstate miss percentages

--- images/idle-governor/idle-governor-cstate-miss-bar.py
+++ images/idle-governor/idle-governor-cstate-miss-bar.py
@@ -10,27 +10,30 @@ FILE_DATA = FILE_BASE + ".txt"
 FILE_PNG  = FILE_BASE + ".png"
 FILE_PDF  = FILE_BASE + ".pdf"
 
-colors = ['#3498db', '#e74c3c']
-lineprops = {"linewidth": 1, "edgecolor": "white"}
-
 df = pd.read_csv(FILE_DATA, delim_whitespace=True, usecols=['C-State', 'Miss', 'Below'])
 df = df.replace(to_replace='-', value=0)
 df['Below'] = pd.to_numeric(df['Below'], errors='coerce')
 
 #grouped_data = df.groupby(by=['C-State']).sum().reset_index()
 
+def calc_perc(part, entries):
+    return 100 * float(part)/float(entries)
+
 grouped_data = df.groupby(by='C-State').agg({'Miss' : 'sum', 'Below' : 'sum', 'C-State' : 'size'})
 grouped_data = grouped_data.rename(columns={'C-State': 'Entries'}).reset_index()
 grouped_data['Above'] = grouped_data['Miss'] - grouped_data['Below']
 grouped_data['Match'] = grouped_data['Entries'] - grouped_data['Miss']
-grouped_data = grouped_data.drop(columns = ['Miss', 'Entries'])
+grouped_data['Above_perc'] = 100 * grouped_data['Above'] / grouped_data['Entries']
+grouped_data['Match_perc'] = 100 * grouped_data['Match'] / grouped_data['Entries']
+grouped_data['Below_perc'] = 100 * grouped_data['Below'] / grouped_data['Entries']
+grouped_data = grouped_data.drop(columns = ['Miss'])
 
 
 # Creating a sorted 90% rotated bar chart
 fig, ax = plt.subplots()
-bars_match = ax.bar(grouped_data['C-State'], grouped_data['Match'])
-bars_above = ax.bar(grouped_data['C-State'], grouped_data['Above'], bottom=grouped_data['Match'])
-bars_below = ax.bar(grouped_data['C-State'], grouped_data['Below'], bottom=(grouped_data['Match'] + grouped_data['Above']))
+bars_match = ax.bar(grouped_data['C-State'], grouped_data['Match_perc'])
+bars_above = ax.bar(grouped_data['C-State'], grouped_data['Above_perc'], bottom=grouped_data['Match_perc'])
+bars_below = ax.bar(grouped_data['C-State'], grouped_data['Below_perc'], bottom=(grouped_data['Match_perc'] + grouped_data['Above_perc']))
 
 # Format axes
 ax.spines['top'].set_visible(False)
@@ -43,7 +46,7 @@ for bar_match, bar_above, bar_below in bars_match, bars_above, bars_below:
 """
 
 plt.xlabel('C-State')
-plt.ylabel('Entries')
+plt.ylabel('Percentage')
 plt.legend(['Match', 'Above', 'Below'])
 
 print(f'generate {FILE_PDF}')
