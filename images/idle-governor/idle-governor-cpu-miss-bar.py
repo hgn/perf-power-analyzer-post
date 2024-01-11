609113e4e3bb42c731c44f34cc40c57a77041a9b
sploityx <florian.aschenbrenner@posteo.de>
2024-01-11 11:54:31 +0100

    idle-governor: added cpu cstate miss bar

--- images/idle-governor/idle-governor-cpu-miss-bar.py
+++ images/idle-governor/idle-governor-cpu-miss-bar.py
@@ -10,12 +10,12 @@ FILE_DATA = FILE_BASE + ".txt"
 FILE_PNG  = FILE_BASE + ".png"
 FILE_PDF  = FILE_BASE + ".pdf"
 
-df = pd.read_csv(FILE_DATA, delim_whitespace=True, usecols=['C-State', 'Miss', 'Below'])
+df = pd.read_csv(FILE_DATA, delim_whitespace=True, usecols=['CPU', 'Miss', 'Below'])
 df = df.replace(to_replace='-', value=0)
 df['Below'] = pd.to_numeric(df['Below'], errors='coerce')
 
-grouped_data = df.groupby(by='C-State').agg({'Miss' : 'sum', 'Below' : 'sum', 'C-State' : 'size'})
-grouped_data = grouped_data.rename(columns={'C-State': 'Entries'}).reset_index()
+grouped_data = df.groupby(by='CPU').agg({'Miss' : 'sum', 'Below' : 'sum', 'CPU' : 'size'})
+grouped_data = grouped_data.rename(columns={'CPU': 'Entries'}).reset_index()
 grouped_data['Above'] = grouped_data['Miss'] - grouped_data['Below']
 grouped_data['Match'] = grouped_data['Entries'] - grouped_data['Miss']
 grouped_data['Above_perc'] = 100 * grouped_data['Above'] / grouped_data['Entries']
@@ -23,10 +23,11 @@ grouped_data['Match_perc'] = 100 * grouped_data['Match'] / grouped_data['Entries
 grouped_data['Below_perc'] = 100 * grouped_data['Below'] / grouped_data['Entries']
 grouped_data = grouped_data.drop(columns = ['Miss'])
 
+
 fig, ax = plt.subplots()
-bars_match = ax.bar(grouped_data['C-State'], grouped_data['Match_perc'])
-bars_above = ax.bar(grouped_data['C-State'], grouped_data['Above_perc'], bottom=grouped_data['Match_perc'])
-bars_below = ax.bar(grouped_data['C-State'], grouped_data['Below_perc'], bottom=(grouped_data['Match_perc'] + grouped_data['Above_perc']))
+bars_match = ax.bar(grouped_data['CPU'], grouped_data['Match_perc'])
+bars_above = ax.bar(grouped_data['CPU'], grouped_data['Above_perc'], bottom=grouped_data['Match_perc'])
+bars_below = ax.bar(grouped_data['CPU'], grouped_data['Below_perc'], bottom=(grouped_data['Match_perc'] + grouped_data['Above_perc']))
 
 for i, bar_match in enumerate(bars_match):
     if grouped_data['Match'][i] > 0:
@@ -51,7 +52,7 @@ ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 
 
-plt.xlabel('C-State')
+plt.xlabel('CPU')
 plt.ylabel('Percentage')
 plt.legend(['Match', 'Above', 'Below'])
 
