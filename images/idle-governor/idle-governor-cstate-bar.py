a548b174d067adf8dbc1eab4487c5b4d3c2f5835
sploityx <florian.aschenbrenner@posteo.de>
2024-01-16 17:40:11 +0100

    idle-governor: add prototype of residency import

--- images/idle-governor/idle-governor-cstate-bar.py
+++ images/idle-governor/idle-governor-cstate-bar.py
@@ -3,28 +3,44 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import json
 
-# Path to the file
-file_path = 'idle-governor-cstate-bar.txt'
+FILE_BASE = os.path.splitext(__file__)[0]
+FILE_DATA = FILE_BASE + ".txt"
+FILE_PNG  = FILE_BASE + ".png"
+FILE_PDF  = FILE_BASE + ".pdf"
+FILE_JSON = FILE_BASE + ".json"
+
+
 
 # Read data from file
-df = pd.read_csv(file_path, delim_whitespace=True)
-
-# Drop unneccesarry columns
-df = df.drop(columns=['Time-Start', 'Delta[ns]', 'Miss', 'CPU'])
+df = pd.read_csv(FILE_DATA, delim_whitespace=True, usecols=['C-State', 'Sleep[ns]'])
+residencies = pd.read_json(FILE_JSON)
+with open(FILE_JSON, 'r') as file:
+    residencies = json.load(file)
+RES_CPU = 'cpu0'
+print(f'WARNING: only using residencies of {RES_CPU}!')
+residencies = residencies[RES_CPU]
 
 # Group by C-State and sum the sleeping times
 grouped_data = df.groupby(['C-State']).sum('Sleep[ns]').reset_index()
+for res in residencies.items():
+    grouped_data.loc[grouped_data['C-State'] == res[1]['name'], 'Residency'] = res[1]['residency']
+#TODO: remove columns drop, if residency has been visualized correctly
+grouped_data = grouped_data.drop(columns='Residency')
 
-# Creating labels for the bar chart by combining TimerType and Callback
-grouped_data['Label'] = str(grouped_data['C-State'])
-
-# Sort the data by Expires in descending order
+# Sort the data by Sleep Time in descending order
 sorted_data = grouped_data.sort_values(by='Sleep[ns]', ascending=False)
+
+# Parameters for Grouped Barchart
+width = 0.4
 
 # Creating a sorted 90% rotated bar chart with logarithmic scale
 fig, ax = plt.subplots()
-bars = ax.barh(sorted_data['C-State'], sorted_data['Sleep[ns]'], log=True)
+ax.barh(sorted_data['C-State'], sorted_data['Sleep[ns]'], width, log=True, color='orange')
+#TODO: use residency time for second bar
+ax.barh(sorted_data['C-State'], sorted_data['Sleep[ns]'], width/2, log=True, color='green')
 
 # format axes
 ax.spines['top'].set_visible(False)
@@ -36,5 +52,8 @@ for c in ax.containers:
 plt.xlabel('Sleep Time [ns]')
 plt.ylabel('C-State')
 plt.gca().invert_yaxis()
-plt.savefig('idle-governor-cstate-bar.pdf')
-plt.savefig('idle-governor-cstate-bar.png')
+
+print(f'generate {FILE_PDF}')
+plt.savefig(FILE_PDF, dpi=300, bbox_inches='tight')
+print(f'generate {FILE_PNG}')
+plt.savefig(FILE_PNG, dpi=300, bbox_inches='tight')
