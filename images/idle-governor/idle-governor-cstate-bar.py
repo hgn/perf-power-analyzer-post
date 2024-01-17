2231aeb9545306da2c4ac92830c0220a6eecdab4
sploityx <florian.aschenbrenner@posteo.de>
2024-01-17 11:17:04 +0100

    idle-governor: added residency for bar

--- images/idle-governor/idle-governor-cstate-bar.py
+++ images/idle-governor/idle-governor-cstate-bar.py
@@ -24,34 +24,38 @@ print(f'WARNING: only using residencies of {RES_CPU}!')
 residencies = residencies[RES_CPU]
 
 # Group by C-State and sum the sleeping times
-grouped_data = df.groupby(['C-State']).sum('Sleep[ns]').reset_index()
+grouped_data = df.groupby(by='C-State').agg({'Sleep[ns]' : 'sum', 'C-State' : 'size'})
+grouped_data = grouped_data.rename(columns={'C-State' : 'Entries'}).reset_index()
+#grouped_data = df.groupby(['C-State']).sum('Sleep[ns]').reset_index()
 for res in residencies.items():
-    grouped_data.loc[grouped_data['C-State'] == res[1]['name'], 'Residency'] = res[1]['residency']
-#TODO: remove columns drop, if residency has been visualized correctly
-grouped_data = grouped_data.drop(columns='Residency')
+    grouped_data.loc[grouped_data['C-State'] == res[1]['name'], 'Residency'] = int(res[1]['residency'])
 
 # Sort the data by Sleep Time in descending order
 sorted_data = grouped_data.sort_values(by='Sleep[ns]', ascending=False)
 
 # Parameters for Grouped Barchart
 width = 0.4
+sleep_colors = plt.cm.summer(np.linspace(0.1, 0.9, len(grouped_data)))
+res_colors = plt.cm.winter(np.linspace(0.1, 0.9, len(grouped_data)))
 
 # Creating a sorted 90% rotated bar chart with logarithmic scale
 fig, ax = plt.subplots()
-ax.barh(sorted_data['C-State'], sorted_data['Sleep[ns]'], width, log=True, color='orange')
-#TODO: use residency time for second bar
-ax.barh(sorted_data['C-State'], sorted_data['Sleep[ns]'], width/2, log=True, color='green')
+sleep_bar = ax.barh(sorted_data['C-State'], sorted_data['Sleep[ns]'], width, log=True, label='Total Sleep', color=sleep_colors)
+res_bar = ax.barh(sorted_data['C-State'], sorted_data['Residency']*1000*sorted_data['Entries'], width/4, log=True, label='Total Residency', color=res_colors)
 
 # format axes
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 
-for c in ax.containers:
-    ax.bar_label(c, fmt='{:,.0f}')
+#for c in ax.containers:
+#    ax.bar_label(c, fmt='{:,.0f}')
+#ax.bar_label(sleep_bar, fmt='{:,.0f}')
+#ax.bar_label(res_bar, label_type='edge', fmt='{:,.0f}')
 
-plt.xlabel('Sleep Time [ns]')
+
+plt.xlabel('Time [ns]')
 plt.ylabel('C-State')
-plt.gca().invert_yaxis()
+plt.legend()
 
 print(f'generate {FILE_PDF}')
 plt.savefig(FILE_PDF, dpi=300, bbox_inches='tight')
