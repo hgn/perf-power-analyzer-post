b28a7deac26e66a93d7fd241b2d5357994ce12de
sploityx <florian.aschenbrenner@posteo.de>
2024-01-18 14:46:11 +0100

    idle-governor: heatmap added pads

--- images/idle-governor/idle-governor-cstate-heatmap.py
+++ images/idle-governor/idle-governor-cstate-heatmap.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
+import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import os
-import json
 
 FILE_BASE = os.path.splitext(__file__)[0]
 FILE_DATA = FILE_BASE + ".txt"
@@ -14,15 +13,9 @@ FILE_JSON = FILE_BASE + ".json"
 
 
 # Read data from file
-df = pd.read_csv(FILE_DATA, delim_whitespace=True, usecols=['C-State', 'Sleep[ns]', 'Miss'])
+df = pd.read_csv(FILE_DATA, delim_whitespace=True, usecols=['C-State', 'Sleep[ns]', 'CPU'])
 residencies = pd.read_json(FILE_JSON)
 
-with open(FILE_JSON, 'r') as file:
-    residencies = json.load(file)
-
-RES_CPU = 'cpu0'
-print(f'WARNING: only using residencies of {RES_CPU}!')
-residencies = residencies[RES_CPU]
 
 # Calculates the optimal matching C-State
 for i in range(len(df)):
@@ -31,7 +24,7 @@ for i in range(len(df)):
     # The smallest residency is always 0
     opt_res_time = -1
     opt_res_name = None
-    for res in residencies.items():
+    for res in residencies['cpu' + str(df.loc[i]['CPU'])].items():
         if int(res[1]['residency'])*1000 <= sleep and int(res[1]['residency']) >= opt_res_time:
             opt_res_time = int(res[1]['residency'])
             opt_res_name = res[1]['name']
@@ -40,9 +33,12 @@ for i in range(len(df)):
     else:
         df.loc[i, 'Optimal-State'] = opt_res_name
 
+
+i_col = sorted(set(df['C-State'].unique()).union(df['Optimal-State'].unique()))
+
 # Correlate Optimal State and Chosen State
-tab = pd.crosstab(df['C-State'], df['Optimal-State'], normalize=0)
-norm_trans = np.array(tab.div(tab.sum(axis=1), axis=0))
+tab = pd.crosstab(df['C-State'], df['Optimal-State']).reindex(index=i_col, columns=i_col, fill_value=0)
+norm_trans = np.array(tab.div(tab.sum(axis=1), axis=0).fillna(0))
 
 # Create heatmap
 fig, ax = plt.subplots()
@@ -52,19 +48,16 @@ im = ax.imshow(norm_trans)
 ax.set_xticks(np.arange(len(tab.axes[1])), labels=tab.axes[1])
 ax.set_yticks(np.arange(len(tab.axes[0])), labels=tab.axes[0])
 
-# Format Axes
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
-
 # Create Annotations
-for i in range(len(df['C-State'].unique())):
-    for j in range(len(df['Optimal-State'].unique())):
+for i in range(len(tab.axes[0])):
+    for j in range(len(tab.axes[1])):
         text = ax.text(j, i, f'{norm_trans[i, j]:.2f}',
                        ha="center", va="center", color="w")
 
 
 plt.xlabel('Optimal C-State')
 plt.ylabel('Chosen C-State')
+plt.gca().invert_yaxis()
 
 print(f'generate {FILE_PDF}')
 plt.savefig(FILE_PDF, dpi=300, bbox_inches='tight')
