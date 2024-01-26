5f09d0e84514156f271e19a98625adc8704f4e68
sploityx <florian.aschenbrenner@posteo.de>
2024-01-26 11:58:01 +0100

    idle-governor: added coloring and legend pie chart

--- images/idle-governor/idle-governor-cstate-pie.py
+++ images/idle-governor/idle-governor-cstate-pie.py
@@ -10,21 +10,27 @@ FILE_DATA = FILE_BASE + ".txt"
 FILE_PNG  = FILE_BASE + ".png"
 FILE_PDF  = FILE_BASE + ".pdf"
 
-colors = ['#3498db', '#e74c3c']
 lineprops = {"linewidth": 1, "edgecolor": "white"}
 
 df = pd.read_csv(FILE_DATA, delim_whitespace=True, usecols=['C-State'])
-grouped_data = df.groupby('C-State')['C-State'].count()
+grouped_data = df.groupby('C-State')['C-State'].count().sort_values(ascending=False)
+print(grouped_data)
 
 def label_print(pct, allvals):
     absolute = int(round(pct/100.*sum(allvals)))
     return "{:.1f}%\n({:d})".format(pct, absolute)
 
 fig, ax = plt.subplots(figsize=(5, 5))
-ax.pie(grouped_data, labels=grouped_data.index,
+
+labels=grouped_data.index
+total_entries = sum(grouped_data.values)
+threshold = (20 * total_entries) / 100
+explode_arr = [0.25 if dat < threshold else 0 for dat in grouped_data.values]
+ax.pie(grouped_data,
         autopct=lambda pct: label_print(pct, grouped_data),
-        startangle=90, colors=colors, wedgeprops=lineprops)
+        startangle=90, wedgeprops=lineprops, pctdistance=1.15, explode=explode_arr)
 ax.axis('equal')
+plt.legend(labels, loc='upper center', bbox_to_anchor=(1, 0, 0.5, 1), ncol=2)
 
 print(f'generate {FILE_PDF}')
 plt.savefig(FILE_PDF, dpi=300, bbox_inches='tight')
