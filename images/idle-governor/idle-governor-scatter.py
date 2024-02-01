6279350ab4285e958c3a76ed0dc56173dd2e8c4e
sploityx <florian.aschenbrenner@posteo.de>
2024-02-01 10:51:38 +0100

    idle-governor: remove unnecessary file constants

--- images/idle-governor/idle-governor-scatter.py
+++ images/idle-governor/idle-governor-scatter.py
@@ -9,8 +9,6 @@ from pathlib import Path
 
 FILE_BASE = os.path.splitext(__file__)[0]
 FILE_DATA = FILE_BASE + ".txt"
-FILE_PNG  = FILE_BASE + ".png"
-FILE_PDF  = FILE_BASE + ".pdf"
 FILE_JSON = FILE_BASE + ".json"
 OUTPUT_FOLDER = "cstates"
 
@@ -23,6 +21,7 @@ def unique_res(residencies, target_cpu=0):
     res = sorted([int(x) for x in res])
     return res
 
+# target_cpu is used for retrieving the residency times
 def graph_scatter_sleeptime_by_sequence(df, graph_cstate, residencies, unique_res, target_cpu=0):
     prev_color = 'red'
     next_color = 'orange'
@@ -59,17 +58,22 @@ def graph_scatter_sleeptime_by_sequence(df, graph_cstate, residencies, unique_re
     if next_res is not None:
         plt.plot([0, len(df)], [next_res, next_res], color=next_color, alpha=0.5,
                  label="Next Residency", linestyle="dotted")
+
     plt.xlabel("Sequence")
     plt.ylabel("Idle Time (" + chr(181) + "s)")
     plt.grid(linewidth=0.3)
 
     plt.yscale("log")
     plt.legend()
+
     ax = plt.subplot(111)
     ax.spines["right"].set_visible(False)
     ax.spines["top"].set_visible(False)
     ax.set_xlim(left=0)
-    plt.savefig(f'cstates/scatter-{cstate}.png', dpi=300)
+
+    file_name = f'{OUTPUT_FOLDER}/scatter-{cstate}.png'
+    print(f'generate {file_name}')
+    plt.savefig(file_name, dpi=300)
     plt.close()
 
 if __name__ == '__main__':
@@ -77,8 +81,8 @@ if __name__ == '__main__':
     df = pd.read_csv(FILE_DATA, delim_whitespace=True, usecols=['C-State', 'Sleep[ns]', 'Miss', 'Below'])
     with open(FILE_JSON, 'r') as res_file:
         residencies = json.load(res_file)
-    Path(OUTPUT_FOLDER).mkdir(parents=True, exist_ok=True)
     unique_res = unique_res(residencies)
+    Path(OUTPUT_FOLDER).mkdir(parents=True, exist_ok=True)
     # Create Graphs
     for cstate in df['C-State'].unique():
         graph_scatter_sleeptime_by_sequence(df, cstate, residencies, unique_res)
