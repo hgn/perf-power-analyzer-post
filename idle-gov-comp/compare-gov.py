90b21e203733d7e58c2a34b2dd06ca50a429578e
sploityx <florian.aschenbrenner@posteo.de>
2024-02-21 14:03:39 +0100

    idle-gov-comp: add basic extended metric

--- idle-gov-comp/compare-gov.py
+++ idle-gov-comp/compare-gov.py
@@ -32,12 +32,22 @@ def read_json(file_name : str):
     return data
 
 
-def save_perf(data : list, file_name : str):
-     with open(file_name, 'w', encoding='utf-8') as json_file:
+def save_json(data : list, file_name : str):
+    '''Saves the data into a JSON'''
+    with open(file_name, 'w', encoding='utf-8') as json_file:
         json.dump(data, json_file, indent=2)
 
 
-def calc_perf(gov_data : pd.DataFrame, res : dict, cstate : str):
+def unique_res(residencies, target_cpu=0):
+    res_dict = residencies[list(residencies.keys())[target_cpu]]
+    res = set()
+    for state in res_dict.values():
+        res.add(state['residency'])
+    res = sorted([int(x) for x in res])
+    return res
+
+
+def simple_perf(gov_data : pd.DataFrame, cstate : str):
     '''Returns the estimated performance of an idle-governor'''
     gov_data = gov_data[gov_data['C-State'] == cstate].reset_index(drop=True)
     if len(gov_data) == 0:
@@ -45,10 +55,47 @@ def calc_perf(gov_data : pd.DataFrame, res : dict, cstate : str):
     perf = 0
     for _, row in gov_data.iterrows():
         if row['Miss'] == 1:
-            if row['Below'] == 0:
+            if row['Below'] == '0':
                 perf -= ABOVE_PEN_WEIGHT
             else:
                 perf -= BELOW_PEN_WEIGHT
+        else:
+            perf += 1
+    perf = perf / len(gov_data)
+    return perf
+
+
+def find_res(res : list, cstate : str, below : bool, target_cpu :int = 0):
+    '''Returns the residency time below or above the cstate'''
+    res_dict = res[list(res.keys())[target_cpu]]
+    res = unique_res(res)
+    target_res = None
+
+    for res_state in res_dict.values():
+        if cstate == res_state['name']:
+            target_res = int(res_state['residency'])
+            break
+
+    index = res.index(int(target_res))
+    if not below:
+        return res[index - 1] if index > 0 else 0
+    return res[index + 1] if index < len(res) - 1 else res[index]
+
+
+
+def extended_perf(gov_data : pd.DataFrame, res : dict, cstate : str):
+    '''Returns the estimated performance of an idle-governor
+    The extended version utilizes the delta of the sleep and the residency bounds'''
+    gov_data = gov_data[gov_data['C-State'] == cstate].reset_index(drop=True)
+    if len(gov_data) == 0:
+        return None
+    perf = 0
+    for _, row in gov_data.iterrows():
+        if row['Miss'] == 1:
+            bound_res = find_res(res, cstate, row['Below'] == '1')
+            weight = (BELOW_PEN_WEIGHT if row['Below'] == '1' else ABOVE_PEN_WEIGHT)
+            #TODO: normalize the delta
+            perf += 1 - weight * (bound_res * 1000 - row['Sleep[ns]'])
         else:
             perf += 1
     perf = perf / len(gov_data)
@@ -63,13 +110,23 @@ def main():
         res = read_json(RES_FILE)
         cstates = gov_data['C-State'].unique()
         for cstate in cstates:
+            perf_simple = simple_perf(gov_data, cstate)
+            perf_extended = extended_perf(gov_data, res, cstate)
             perf.append({'Idle-Governor' : gov, 'C-State' : cstate,
-                         'Perf' : calc_perf(gov_data, res, cstate)})
-
-        perf_total = sum(entry['Perf'] for entry in perf if entry.get('Idle-Governor') == gov)
+                         'Perf-Simple' : perf_simple,
+                         'Perf-Extended' : perf_extended,
+                         'Occurences' : len(gov_data[gov_data['C-State'] == cstate])})
+            extended_perf(gov_data, res, cstate)
+        occ_total = sum(entry['Occurences'] for entry in perf if entry.get('Idle-Governor') == gov)
+        perf_simple_total = sum(entry['Perf-Simple']*entry['Occurences']/occ_total
+                                for entry in perf if entry.get('Idle-Governor') == gov)
+        perf_extended_total = sum(entry['Perf-Extended']*entry['Occurences']/occ_total
+                                for entry in perf if entry.get('Idle-Governor') == gov)
         perf.append({'Idle-Governor' : gov, 'C-State' : 'all',
-                     'Perf' : perf_total})
-    save_perf(perf, PERFORMANCE_FILE)
+                     'Perf-Simple' : perf_simple_total,
+                     'Perf-Extended' : perf_extended_total,
+                     'Occurences' : occ_total})
+    save_json(perf, PERFORMANCE_FILE)
 
 
 if __name__ == '__main__':
