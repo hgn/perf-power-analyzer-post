e98df4e83d21e5005f774d9b9f49eed33b66738f
sploityx <florian.aschenbrenner@posteo.de>
2024-02-23 11:17:25 +0100

    idle-gov-comp: add line plot

--- idle-gov-comp/visualize-comp.py
+++ idle-gov-comp/visualize-comp.py
@@ -1,19 +1,54 @@
 #!/usr/bin/env python3
 
+import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import utils
 
-df = pd.DataFrame(utils.read_json('examples/idle-governor-performance.json'))
 
-df = df[df['C-State'] != 'all']
-plt.figure(figsize=(16,10), dpi= 80)
-print(df.loc[df['Idle-Governor'] == 'ladder', 'Perf-Extended'])
-sns.kdeplot(df.loc[df['Idle-Governor'] == 'ladder', 'Perf-Extended'], fill=True, color='g', label='ladder', alpha=.2)
-sns.kdeplot(df.loc[df['Idle-Governor'] == 'teo', 'Perf-Extended'], fill=True, color='deeppink', label='teo', alpha=.2)
-sns.kdeplot(df.loc[df['Idle-Governor'] == 'menu', 'Perf-Extended'], fill=True, color='dodgerblue', label='menu', alpha=.2)
+BASE_FOLDER = 'examples/'
+VIS_FOLDER = BASE_FOLDER + 'visualization/'
+GOVS = ['ladder', 'menu', 'teo']
 
-plt.title('Density Plot of Perf-Extended Grouped by Idle-Governors', fontsize=22)
-plt.legend()
-plt.show()
+
+def save_plt(file_base : str):
+    '''Saves a plot from the plt environment to pdf and png and resets plt afterwards'''
+    print(f'generate {file_base}.pdf')
+    plt.savefig(file_base + '.pdf', dpi=300, bbox_inches='tight')
+    print(f'generate {file_base}.png')
+    plt.savefig(file_base + '.png', dpi=300, bbox_inches='tight')
+    plt.clf()
+
+
+def density_plot(perf_df : pd.DataFrame):
+    '''Generates a density plot in the plt environment'''
+    perf_df = perf_df[perf_df['C-State'] != 'all']
+    colors = ['g', 'deeppink', 'dodgerblue']
+    plt.figure(figsize=(16,10), dpi=300)
+    for col, gov in zip(colors, GOVS):
+        sns.kdeplot(perf_df.loc[perf_df['Idle-Governor'] == gov, 'Perf-Extended'],
+                    fill=True, color=col, label=gov, alpha=.4)
+    plt.title('Density Plot of Perf-Extended Grouped by Idle-Governors', fontsize=22)
+    plt.legend()
+
+
+def line_plot(perf_df : pd.DataFrame):
+    '''Generates a line plot in the plt environment'''
+    perf_df = perf_df[perf_df['C-State'] != 'all']
+    sns.lineplot(data=perf_df, x='C-State', y='Perf-Extended', hue='Idle-Governor')
+
+
+def main():
+    '''Generates and saves plots'''
+    if not os.path.isdir(VIS_FOLDER):
+        os.mkdir(VIS_FOLDER)
+    perf_df = pd.DataFrame(utils.read_json(BASE_FOLDER + 'idle-governor-performance.json'))
+    density_plot(perf_df)
+    save_plt(VIS_FOLDER + 'idle-gov-comp-density')
+    line_plot(perf_df)
+    save_plt(VIS_FOLDER + 'idle-gov-comp-line')
+
+
+if __name__ == '__main__':
+    main()
