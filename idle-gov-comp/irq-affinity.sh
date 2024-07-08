13e3216c61cedd02235b7bf981ee21de72eacb48
sploityx <florian.aschenbrenner@posteo.de>
2024-07-08 13:18:30 +0200

    idle-gov-comp: limit cpus to 0 and 1 for irq

--- idle-gov-comp/irq-affinity.sh
+++ idle-gov-comp/irq-affinity.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-# Migrate irqs to CPU 0-2 (exclude CPU 3)
+# Migrate irqs to CPU 0-1 (exclude CPU 2,3)
 for I in $(ls /proc/irq)
 do
     if [[ -d "/proc/irq/$I" ]]
     then
-        echo "Affining vector $I to CPUs 0-2"
-        echo 0-2 > /proc/irq/$I/smp_affinity_list
+        echo "Affining vector $I to CPUs 0-1"
+        echo 0-1 > /proc/irq/$I/smp_affinity_list
     fi
 done
